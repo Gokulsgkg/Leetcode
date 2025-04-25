8df34ad50b523919f2f17dd4c4916e00cca64e5c
Gokul S <149486301+Gokulsgkg@users.noreply.github.com>
2025-04-25 22:20:30 +0530

    Time: 1 ms (64.23%), Space: 41.1 MB (97.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,11 +3,11 @@ class Solution {
         if(strs.length == 1)return strs[0];
         Arrays.sort(strs);
         StringBuilder sb = new StringBuilder();
-        String s1 = strs[0];
-        String s2 = strs[strs.length-1];
-        for(int i = 0 ; i < s1.length() && i < s2.length() ;i++){
-            if(s1.charAt(i) != s2.charAt(i))break;
-            sb.append(s1.charAt(i));
+        char[] s1 = strs[0].toCharArray();
+        char[] s2 = strs[strs.length-1].toCharArray();
+        for(int i = 0 ; i < s1.length && i < s2.length ;i++){
+            if(s1[i] != s2[i])break;
+            sb.append(s1[i]);
         }
         return sb.toString();
     }
