60cd62877e4b6e5517628379b10e5969634bd9da
Gokul S <149486301+Gokulsgkg@users.noreply.github.com>
2025-05-05 16:17:01 +0530

    Time: 0 ms (100%), Space: 43.5 MB (93.78%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,16 +1,15 @@
 public class Solution {
-    public int[] finalPrices(int[] prices) {
-        Stack<Integer> st = new Stack<>();
-        int n = prices.length;
-
-        for (int i = 0; i < n; i++) {
-            while (!st.isEmpty() && prices[i] <= prices[st.peek()]) {
-                int idx = st.pop();
-                prices[idx] -= prices[i];
-            }
-            st.push(i);
+    static int getDisc(int[] prices , int i , int n){
+        for(int j = i+1 ; j < n ; j++){
+            if(prices[i] >= prices[j])return prices[i]-prices[j];
         }
-
+        return prices[i];
+    }
+    public int[] finalPrices(int[] prices) {
+        int n = prices.length;
+        for(int i = 0 ; i < n ; i++){
+            prices[i] =  getDisc(prices,i,n);
+        }
         return prices;
     }
 }
