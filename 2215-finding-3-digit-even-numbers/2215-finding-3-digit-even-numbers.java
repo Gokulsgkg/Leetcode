583070f5142f570da178934c3bbe7ce2a372a8e7
Gokul S <149486301+Gokulsgkg@users.noreply.github.com>
2025-05-12 20:49:07 +0530

    Time: 3 ms (97.44%), Space: 44.6 MB (82.46%) - LeetHub

--- 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.java
@@ -2,29 +2,39 @@ import java.util.*;
 
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        boolean[] visit = new boolean[1000]; 
-        ArrayList<Integer> ans = new ArrayList<>();
-        Arrays.sort(digits); 
-        for (int i = 0; i < digits.length; i++) {
-            for (int j = 0; j < digits.length; j++) {
-                for (int k = 0; k < digits.length; k++) {
-                    if (i == j || j == k || i == k) continue;
+        int[] freq = new int[10];
+        for (int d : digits) {
+            freq[d]++;
+        }
 
-                    int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+        List<Integer> result = new ArrayList<>();
 
-                    if (digits[i] == 0) continue;
-                    if (num % 2 != 0) continue;
-                    if (visit[num]) continue;
+        for (int num = 100; num < 1000; num += 2) {
+            int a = num / 100;
+            int b = (num / 10) % 10;
+            int c = num % 10;
 
-                    visit[num] = true;
-                    ans.add(num);
+            int[] tempFreq = new int[10];
+            tempFreq[a]++;
+            tempFreq[b]++;
+            tempFreq[c]++;
+
+            boolean valid = true;
+            for (int i = 0; i < 10; i++) {
+                if (tempFreq[i] > freq[i]) {
+                    valid = false;
+                    break;
                 }
             }
+
+            if (valid) result.add(num);
         }
-        int[] res = new int[ans.size()];
-        for (int i = 0; i < ans.size(); i++) {
-            res[i] = ans.get(i);
+
+        int[] res = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            res[i] = result.get(i);
         }
+
         return res;
     }
 }
