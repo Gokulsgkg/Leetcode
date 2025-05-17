5ca3bcc8bc132036bb8b1752a0b7f36650c5fce2
Gokul S <149486301+Gokulsgkg@users.noreply.github.com>
2025-05-17 22:27:53 +0530

    Time: 1 ms (99.68%), Space: 42.2 MB (66.39%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,31 +1,17 @@
-import java.math.BigInteger;
 class Solution {
-    static int digit(char[] a) {
-        long pow = 1;
-        long ans = 0;
-        for (int i = a.length - 1; i >= 0; i--) {
-            if (a[i] == '1') {
-                ans += pow;
-            }
-            pow *= 2;
-        }
-        return (int)ans;
-    }
-
-    static String binary(int ans) {
-        if (ans == 0) return "0";
+    public String addBinary(String a, String b) {
         StringBuilder sb = new StringBuilder();
-        while (ans != 0) {
-            sb.append(ans % 2);
-            ans /= 2;
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int carry = 0;
+        
+        while(i >= 0 || j >= 0 || carry != 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--)-'0';
+            if(j >= 0) sum += b.charAt(j--)-'0';
+            sb.append(sum % 2);
+            carry = sum / 2;
         }
         return sb.reverse().toString();
-    }
-
-    public String addBinary(String a, String b) {
-        BigInteger num1 = new BigInteger(a, 2);
-        BigInteger num2 = new BigInteger(b, 2);
-        BigInteger sum = num1.add(num2);
-        return sum.toString(2); 
     }
 }
