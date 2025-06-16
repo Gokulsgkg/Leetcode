4f3cc2d70b29b50ff08a934872b8874696f886a2
Gokul S <149486301+Gokulsgkg@users.noreply.github.com>
2025-06-16 22:57:12 +0530

    Time: 0 ms (100%), Space: 48.3 MB (14.66%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,35 +1,21 @@
 class Solution {
-    private static void dfs(int node , List<Integer> adjList[] , boolean[] vis){
+    private static void dfs(int node , int[][] isConnected , boolean[] vis){
         vis[node] = true;
-        for(int it : adjList[node]){
-            if(!vis[it]){
-                dfs(it , adjList , vis);
+        for(int i = 0 ; i < isConnected.length ; i++){
+            if(isConnected[node][i] == 1 && !vis[i]){
+                dfs(i, isConnected , vis);
             }
         }
     }
     public int findCircleNum(int[][] isConnected) {
         int V = isConnected.length;
- 
-        List<Integer>[] adjList = new ArrayList[V];
-        for(int i = 0 ; i < V ; i++){
-            adjList[i] = new ArrayList<>();
-        }
-
-        for(int i = 0 ; i < V ; i++){
-            for(int j = 0 ; j < V ; j++){
-                if(isConnected[i][j] == 1 && i != j){
-                    adjList[i].add(j);
-                    adjList[j].add(i);
-                }
-            }
-        }
 
         boolean[] vis = new boolean[V];
         int count = 0;
         for(int i = 0 ; i < V ; i++){
             if(!vis[i]){
                 count++;
-                dfs(i , adjList , vis);
+                dfs(i , isConnected , vis);
             }
         }
         return count;
